6ba75b0d4bea79ad7f9e36f5aab8f8611d1abe40
Peng <94596408+peng-few@users.noreply.github.com>
2023-10-02 15:29:39 +0800

    fix: remove temp code

--- app/food/page.tsx
+++ app/food/page.tsx
@@ -2,10 +2,8 @@ import FoodAdd from "./FoodAdd";
 import FoodTable from "./FoodTable";
 import Typography from "@mui/material/Typography";
 import BrandsProvider from "@/brand/_components/BrandsProvider";
-import { revalidateTag } from "next/cache";
 
 export default async function Food() {
-  revalidateTag('brands')
   return (
     <div className="py-4 px-6">
       <Typography className="pb-3" variant="h1" display="block">
--- app/record/page.tsx
+++ app/record/page.tsx
@@ -3,12 +3,10 @@ import Typography from "@mui/material/Typography";
 import RecordAdd from "./RecordAdd";
 import FoodsProvider from "@/food/_components/FoodsProvider";
 import RecordTable from "./RecordTable";
-import { revalidateTag } from "next/cache";
 import { Suspense } from "react";
 import { Loading } from "@/_components";
 
 export default async function Record() {
-  revalidateTag('records')
   return (
     <div className="py-4 px-6">
       <Typography className="pb-3" variant="h1" display="block">
