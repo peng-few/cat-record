d5ec2bf9d22f7df8000a11bfe53599c3b0d4bad0
Peng <94596408+peng-few@users.noreply.github.com>
2023-10-04 14:31:01 +0800

    bugfix

--- app/_lib/__tests__/unitConverter.test.ts
+++ app/_lib/__tests__/unitConverter.test.ts
@@ -6,7 +6,7 @@ import unitConverter from '../unitConverter'
  
 describe('unitConverter', () => {
   it('convert heat to Percentage correctly', () => {
-    expect(unitConverter.heatToPercentage(125,111.7)).toBe(0.14)
+    expect(unitConverter.mgToPercentage(125,111.7)).toBe(0.14)
   })
   it('convert value to percentage correctly', () => {
     expect(unitConverter.toPercentage(12.25)).toBe(0.12)
